b2787173fc7f0691294eb54afa6a6a608b159bdb
Santino Ventrice <santino.ventrice@gmail.com>
2025-05-27 12:48:31 -0300

    Agregando control de stock

--- kiosco.py
+++ kiosco.py
@@ -1,18 +1,18 @@
 # Inicialización de variable
 comando = ""
 
-# Lista de productos
+# Lista de productos [ID, Nombre, Precio, Cantidad]
 productos = [
-    ["1", "Galletitas Oreo", 550],
-    ["2", "Chupetín Pico Dulce", 100],
-    ["3", "Alfajor Jorgito", 300],
-    ["4", "Coca-Cola 500ml", 750],
-    ["5", "Papas Lays", 800],
-    ["6", "Chicle Topline", 150],
-    ["7", "Bon o Bon", 200],
-    ["8", "Agua mineral 500ml", 500],
-    ["9", "Tita", 180],
-    ["10", "Palito helado", 600],
+    ["1", "Galletitas Oreo", 550, 55],
+    ["2", "Chupetín Pico Dulce", 100, 80],
+    ["3", "Alfajor Jorgito", 300, 120],
+    ["4", "Coca-Cola 500ml", 750, 100],
+    ["5", "Papas Lays", 800, 150],
+    ["6", "Chicle Topline", 150, 40],
+    ["7", "Bon o Bon", 200, 90],
+    ["8", "Agua mineral 500ml", 500, 70],
+    ["9", "Tita", 180, 60],
+    ["10", "Palito helado", 600, 200],
 ]
 
 # Lista de carrito
@@ -33,12 +33,19 @@ def lista_productos():
         if id == "salir":
             print("\nSaliendo de la lista de productos...\n")
         else:
-            # Busca por cada producto si el ID coincide con el que ingresó el usuario
+            # Busca en la lista de productos producto por producto hasta que el ID ingresado coincida con el ID de algún producto
             for producto in productos:
-                # Si coincide, lo agrega al carrito
                 if id == producto[0]:
-                    cantidad = int(input('Ingresar cantidad de este producto: '))
-                    carrito.append((cantidad, producto[1]))
+                    cantidad = int(input(f'Ingresar cantidad de este producto (Unidades restantes: {producto[3]}): '))
+
+                    # Si el usuario pide más de la cantidad disponible o 0
+                    while cantidad > producto[3] or cantidad == 0:
+                        print(f"Ingresar una cantidad válida.")
+                        cantidad = int(input(f'Ingresar cantidad de este producto (Unidades restantes: {producto[3]}): '))
+
+                    # Agregar al carrito
+                    carrito.append((cantidad, producto[1], producto[2]))
+                    print(f"Producto {producto[1]} agregado al carrito.")
 
 # Función para ver el carrito
 def ver_carrito():
